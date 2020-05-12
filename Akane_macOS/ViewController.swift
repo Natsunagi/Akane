a5782388f86ce2501de796108311b2c7d4b1b471
WangYi <rd331@netvox.com.cn>
2020-05-12 16:17:29 +0800

    视图控制器初始化。

---
+++ Akane_macOS/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Akane_macOS
+//
+//  Created by Grass Plainson on 2020/5/10.
+//  Copyright © 2020 Grass Plainson. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
