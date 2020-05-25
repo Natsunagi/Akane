040ae153a732779740d9653e0431ebf1b1af5ea8
WangYi <rd331@netvox.com.cn>
2020-05-25 14:51:07 +0800

    对 iCloud 进行文件的写入、读取，以及将本地文件移动到 iCloud 中（失败）。

---
+++ Akane/测试模块/iCloudAndFiles/AKAppleCloudFilesOperationViewController.swift
@@ -0,0 +1,72 @@
+//
+//  AKAppleCloudFilesOperationViewController.swift
+//  Akane
+//
+//  Created by Grass Plainson on 2020/5/25.
+//  Copyright © 2020 Grass Plainson. All rights reserved.
+//
+
+import UIKit
+
+class AKAppleCloudFilesOperationViewController: UIViewController, NSFilePresenter {
+    var presentedItemURL: URL?
+    
+    var presentedItemOperationQueue: OperationQueue = OperationQueue.main
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        
+        // MARK: 在 iCloud 上创建一个数据文件夹。
+        
+        if let iCloudUrl = AKManager.iCloudUrl {
+            do {
+                try FileManager.default.createDirectory(at: iCloudUrl.appendingPathComponent("UserData"), withIntermediateDirectories: true, attributes: nil)
+                self.presentedItemURL = iCloudUrl.appendingPathComponent("UserData")
+            } catch {
+                print(error.localizedDescription)
+            }
+        }
+        
+        // MARK: 创建一个测试的 plist 文件。
+        
+        let dictionary: NSDictionary = [
+            "name": "wangyi",
+            "age": 23
+        ]
+        let documentPath: String = NSSearchPathForDirectoriesInDomains(.documentDirectory, .userDomainMask, true).first!
+        let savePath: String = documentPath.appending("/info.plist")
+        dictionary.write(toFile: savePath, atomically: true)
+        
+        // MARK: 移动文件到 iCloud。
+        
+        do {
+            try FileManager.default.setUbiquitous(true, itemAt: URL.init(string: savePath)!, destinationURL: AKManager.iCloudUrl!.appendingPathComponent("UserData").appendingPathComponent("info.plist"))
+        } catch {
+            print(error.localizedDescription)
+        }
+        
+        // MARK: 将文件写入到 iCloud。
+        
+        NSFileCoordinator.addFilePresenter(self)
+        let fileCoordinator: NSFileCoordinator = NSFileCoordinator.init(filePresenter: self)
+        var error: NSError? = NSError.init()
+        fileCoordinator.coordinate(writingItemAt: AKManager.iCloudUrl!.appendingPathComponent("UserData"), options: .forReplacing, error: &error) { (url) in
+            dictionary.write(to: url.appendingPathComponent("info.plist"), atomically: true)
+        }
+        NSFileCoordinator.removeFilePresenter(self)
+        
+        // MARK: 读取文件。
+        
+        fileCoordinator.coordinate(readingItemAt: AKManager.iCloudUrl!.appendingPathComponent("UserData").appendingPathComponent("info.plist"), options: .withoutChanges, error: &error) { (url) in
+            let result: NSDictionary = NSDictionary.init(contentsOf: url)!
+            let name: String = result["name"] as! String
+            let age: Int = result["age"] as! Int
+            print(name)
+            print(age)
+        }
+    }
+
+}
