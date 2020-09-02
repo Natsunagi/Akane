cf94c113a9b4743983e662a19870bc2c5f730568
WangYi <rd331@netvox.com.cn>
2020-09-02 13:31:07 +0800

    图片上添加手势操作优化。

--- Akane/测试模块/功能模块/Image/AKImageGestureViewController.swift
+++ Akane/测试模块/功能模块/Image/AKImageGestureViewController.swift
@@ -18,6 +18,8 @@ class AKImageGestureViewController: UIViewController, UIScrollViewDelegate {
     private var beginPoint: CGPoint = CGPoint.init(x: 0, y: 0)
     
     var markerView: UIView!
+        
+    static let markerDidChangeLocation: Notification.Name = Notification.Name.init("markerDidChangeLocation")
     
     // MARK: - UIViewController.
     
@@ -25,6 +27,10 @@ class AKImageGestureViewController: UIViewController, UIScrollViewDelegate {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        
+        NotificationCenter.default.addObserver(self, selector: #selector(self.handleMarkerChangeLocation(notification:)), name: AKImageGestureViewController.markerDidChangeLocation, object: nil)
+        
+        self.navigationItem.rightBarButtonItem = UIBarButtonItem.init(title: "清除", style: .plain, target: self, action: #selector(self.clearAllMarker))
         
         self.scrollView.minimumZoomScale = 1
         self.scrollView.maximumZoomScale = 2
@@ -42,64 +48,73 @@ class AKImageGestureViewController: UIViewController, UIScrollViewDelegate {
             view!.top.equalTo()(self.scrollView.mas_top)?.offset()
             view!.bottom.equalTo()(self.scrollView.mas_bottom)?.offset()
         }
+        self.imageView.contentMode = .bottomLeft
         let tapGesture: UITapGestureRecognizer = UITapGestureRecognizer.init(target: self, action: #selector(self.handleTapGesture(gesture:)))
         tapGesture.delegate = self
         self.imageView.addGestureRecognizer(tapGesture)
+    }
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
         
-        // MARK: - 拖动视图手势添加。
+        self.scrollView.contentSize = self.imageView.image!.size
         
-        self.markerView = UIView.init()
-        self.markerView.isUserInteractionEnabled = true
-        self.markerView.backgroundColor = .clear
-        let drawImageView: UIImageView = UIImageView.init()
-        drawImageView.isUserInteractionEnabled = true
-        self.markerView.addSubview(drawImageView)
+        // - 读取本地标记文件。
         
-        let baseStationDiameter: CGFloat = 70  // 直径
-        
-        self.markerView.frame = CGRect.init(x: 160, y: 300, width: baseStationDiameter, height: baseStationDiameter)
-        
-        drawImageView.frame = CGRect.init(x: 0, y: 0, width: 70, height: 70)
-        
-        UIGraphicsBeginImageContextWithOptions(CGSize.init(width: 70, height: 70), false, 0)
-        if let context = UIGraphicsGetCurrentContext() {
-            context.setStrokeColor(UIColor.red.cgColor)
-            context.setFillColor(red: 1, green: 0, blue: 0, alpha: 0.5)
-            context.addEllipse(in: CGRect.init(x: 0, y: 0, width: 70, height: 70))
-            context.setLineWidth(2)
-            context.fillPath()
-            context.setFillColor(red: 1, green: 0, blue: 0, alpha: 1)
-            context.addArc(center: CGPoint.init(x: 35, y: 35), radius: 10, startAngle: 0, endAngle: 2 * CGFloat.pi, clockwise: true)
-            context.fillPath()
+        let documentsPath: String = NSSearchPathForDirectoriesInDomains(.documentDirectory, .userDomainMask, true).first!
+        let savePath: String = documentsPath.appending("/Location.plist")
+        let array: NSArray? = NSArray.init(contentsOfFile: savePath)
+        if array == nil {
+            return
         }
-        drawImageView.image = UIGraphicsGetImageFromCurrentImageContext()
-        UIGraphicsEndImageContext()
-        
-        let panGesture: UIPanGestureRecognizer = UIPanGestureRecognizer.init(target: self, action: #selector(self.handlePanGesture(gesture:)))
-        panGesture.delegate = self
-        self.markerView.addGestureRecognizer(panGesture)
-        self.imageView.addSubview(self.markerView)
+        for object in array! {
+            let dictionary: Dictionary<String, Float> = object as! Dictionary<String, Float>
+            let x: Float = dictionary["x"]!
+            let y: Float = dictionary["y"]!
+            self.imageView.addSubview(AKMarkerView.init(x: CGFloat(x) * self.imageView.image!.size.width, y: CGFloat(y) * self.imageView.image!.size.height))
+        }
     }
     
     // MARK: - Handle gesture.
     
-    @objc private func handlePanGesture(gesture: UIGestureRecognizer) {
-        let currentPoint: CGPoint = gesture.location(in: self.markerView)
-        
-        if (gesture.state == UIGestureRecognizer.State.began) {
-            self.beginPoint = gesture.location(in: self.markerView)
-        } else if (gesture.state == UIGestureRecognizer.State.changed) {
-            let offset: CGPoint = CGPoint.init(x: currentPoint.x - self.beginPoint.x, y: currentPoint.y - self.beginPoint.y)
-            self.markerView.frame.origin = CGPoint.init(x: self.markerView.frame.origin.x + offset.x, y: self.markerView.frame.origin.y + offset.y)
-        } else if (gesture.state == UIGestureRecognizer.State.ended) {
-            let offset: CGPoint = CGPoint.init(x: currentPoint.x - self.beginPoint.x, y: currentPoint.y - self.beginPoint.y)
-            self.markerView.frame.origin = CGPoint.init(x: self.markerView.frame.origin.x + offset.x, y: self.markerView.frame.origin.y + offset.y)
-        }
-    }
-    
     @objc private func handleTapGesture(gesture: UIGestureRecognizer) {
         let currentPoint: CGPoint = gesture.location(in: self.imageView)
-        print(currentPoint)
+        
+        // - 添加一个移动标记。传进来的参数是手指的位置，应该也就是圆心的位置。
+        
+        self.imageView.addSubview(AKMarkerView.init(x: currentPoint.x, y: currentPoint.y))
+        
+        // - 保存移动标记位置, 保存的是横纵坐标的百分比。
+        
+        self.saveLocation()
+    }
+    
+    private func saveLocation() {
+        let array: NSMutableArray = NSMutableArray.init()
+        for view in self.imageView.subviews {
+            let markerView: AKMarkerView = view as! AKMarkerView
+            let dictionary: Dictionary<String, Float> = [
+                "x": Float(markerView.currentLocation.x) / Float(self.imageView.image!.size.width),
+                "y": Float(markerView.currentLocation.y) / Float(self.imageView.image!.size.height),
+                "scale": 1.0
+            ]
+            array.add(dictionary)
+        }
+        let documentsPath: String = NSSearchPathForDirectoriesInDomains(.documentDirectory, .userDomainMask, true).first!
+        let savePath: String = documentsPath.appending("/Location.plist")
+        array.write(toFile: savePath, atomically: true)
+    }
+    
+    @objc private func handleMarkerChangeLocation(notification: Notification) {
+        self.saveLocation()
+    }
+    
+    @objc private func clearAllMarker() {
+        for view in self.imageView.subviews {
+            view.isHidden = true
+            view.removeFromSuperview()
+        }
+        self.saveLocation()
     }
     
     // MARK: - UIScrollViewDelegate.
@@ -110,6 +125,75 @@ class AKImageGestureViewController: UIViewController, UIScrollViewDelegate {
 }
 
 extension AKImageGestureViewController: UIGestureRecognizerDelegate {
+    
+    func gestureRecognizer(_ gestureRecognizer: UIGestureRecognizer, shouldBeRequiredToFailBy otherGestureRecognizer: UIGestureRecognizer) -> Bool {
+        if (gestureRecognizer.isKind(of: UIPanGestureRecognizer.classForCoder()) && otherGestureRecognizer.isKind(of: UIPanGestureRecognizer.classForCoder())) {
+            return true
+        } else {
+            return false
+        }
+    }
+}
+
+class AKMarkerView: UIView {
+    
+    private var beginPoint: CGPoint = CGPoint.init(x: 0, y: 0)
+    var currentLocation: CGPoint = CGPoint.init(x: 0, y: 0)
+    
+    private let diameter: CGFloat = 70  // 直径。
+    
+    init(x: CGFloat, y: CGFloat) {
+        super.init(frame: CGRect.init(x: x - self.diameter / 2, y: y - self.diameter / 2, width: self.diameter, height: self.diameter))
+        
+        self.backgroundColor = .clear
+        let drawImageView: UIImageView = UIImageView.init()
+        drawImageView.isUserInteractionEnabled = true
+        self.addSubview(drawImageView)
+        
+        drawImageView.frame = CGRect.init(x: 0, y: 0, width: self.diameter, height: self.diameter)
+                
+        UIGraphicsBeginImageContextWithOptions(CGSize.init(width: self.diameter, height: self.diameter), false, 0)
+        if let context = UIGraphicsGetCurrentContext() {
+            context.setStrokeColor(UIColor.red.cgColor)
+            context.setFillColor(red: 1, green: 0, blue: 0, alpha: 0.5)
+            context.addEllipse(in: CGRect.init(x: 0, y: 0, width: self.diameter, height: self.diameter))
+            context.setLineWidth(2)
+            context.fillPath()
+            context.setFillColor(red: 1, green: 0, blue: 0, alpha: 1)
+            context.addArc(center: CGPoint.init(x: self.diameter / 2, y: self.diameter / 2), radius: 10, startAngle: 0, endAngle: 2 * CGFloat.pi, clockwise: true)
+            context.fillPath()
+        }
+        drawImageView.image = UIGraphicsGetImageFromCurrentImageContext()
+        UIGraphicsEndImageContext()
+                
+        let panGesture: UIPanGestureRecognizer = UIPanGestureRecognizer.init(target: self, action: #selector(self.handlePanGesture(gesture:)))
+        panGesture.delegate = self
+        self.addGestureRecognizer(panGesture)
+        self.currentLocation = CGPoint.init(x: x, y: y)  // 保存的值是圆心点的位置。
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    @objc private func handlePanGesture(gesture: UIGestureRecognizer) {
+        let currentPoint: CGPoint = gesture.location(in: self)
+        
+        if (gesture.state == UIGestureRecognizer.State.began) {
+            self.beginPoint = gesture.location(in: self)
+        } else if (gesture.state == UIGestureRecognizer.State.changed) {
+            let offset: CGPoint = CGPoint.init(x: currentPoint.x - self.beginPoint.x, y: currentPoint.y - self.beginPoint.y)
+            self.frame.origin = CGPoint.init(x: self.frame.origin.x + offset.x, y: self.frame.origin.y + offset.y)
+        } else if (gesture.state == UIGestureRecognizer.State.ended) {
+            let offset: CGPoint = CGPoint.init(x: currentPoint.x - self.beginPoint.x, y: currentPoint.y - self.beginPoint.y)
+            self.frame.origin = CGPoint.init(x: self.frame.origin.x + offset.x, y: self.frame.origin.y + offset.y)
+            self.currentLocation = CGPoint.init(x: self.frame.origin.x + self.diameter / 2, y: self.frame.origin.y + self.diameter / 2)  // 保存的应该是圆心的位置。
+            NotificationCenter.default.post(name: AKImageGestureViewController.markerDidChangeLocation, object: nil)
+        }
+    }
+}
+
+extension AKMarkerView: UIGestureRecognizerDelegate {
     
     func gestureRecognizer(_ gestureRecognizer: UIGestureRecognizer, shouldBeRequiredToFailBy otherGestureRecognizer: UIGestureRecognizer) -> Bool {
         if (gestureRecognizer.isKind(of: UIPanGestureRecognizer.classForCoder()) && otherGestureRecognizer.isKind(of: UIPanGestureRecognizer.classForCoder())) {
